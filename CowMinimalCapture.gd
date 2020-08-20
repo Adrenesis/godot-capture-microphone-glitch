542807ee9d414b2b2e568d7262e0b08b6021fbf3
Adrenesis <adrenesis@gmail.com>
2020-08-20 11:53:19 +0200

    Reduce the number of call to get_recording

--- CowMinimalCapture.gd
+++ CowMinimalCapture.gd
@@ -1,36 +1,42 @@
 extends Node
 
-const MIN_PACKET_LENGTH = 0.25
-const STARTUP_TIME = 5.0
-var mic_number = 300
-var mics = []
+const MIN_PACKET_LENGTH = 0.180
+const STARTUP_TIME = 1.0
+var mic_number = 1
+var record_number = 30
+var mic : AudioEffectRecord
 var time_elapsed_since_record = 0
 var time_elapsed = 0
+var triggering_mic_down = false
+var triggering_mic_up = false
 
 func _ready():
-	for i in range(mic_number):
-		AudioServer.add_bus_effect(1, AudioEffectRecord.new(), i)
-		var _mic = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), i)
-		_mic.set_recording_active(true)
-		mics.push_back(_mic)
+	AudioServer.add_bus_effect(1, AudioEffectRecord.new(), 0)
+	mic = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 0)
+	mic.set_recording_active(true)
 	
 var debugFirstRound = false
 func _process(delta: float) -> void:
-	
 	if time_elapsed >= STARTUP_TIME:
-		if time_elapsed_since_record >= MIN_PACKET_LENGTH:
-			print("reading mics...")
-			for _mic in mics:
-				_mic.set_recording_active(false)
-				var r = _mic.get_recording()
-				_mic.set_recording_active(true)
-				time_elapsed_since_record = 0
-	else:
-		if time_elapsed_since_record >= MIN_PACKET_LENGTH:
-			print("triggering mics...")
-			for _mic in mics:
-				_mic.set_recording_active(false)
-				_mic.set_recording_active(true)
-				time_elapsed_since_record = 0
+		if time_elapsed_since_record >= MIN_PACKET_LENGTH and mic.get_recording_size() > 0:
+			print("reading mics... of " + str(mic.get_recording_size()))
+			for i in range(record_number):
+				if i % 15 == 0:
+					print(i)
+					pass
+				var _r = mic.get_recording()
+			mic.set_recording_active(true) 
+#				yield(get_tree(), "idle_frame")
+				
+			time_elapsed_since_record = 0
+#				r.unreference()
+		triggering_mic_down = true
+#	else:
+#		if time_elapsed_since_record >= MIN_PACKET_LENGTH:
+#			print("triggering mics...")
+#			for _mic in mics:
+#				_mic.set_recording_active(false)
+#				_mic.set_recording_active(true)
+#				time_elapsed_since_record = 0
 	time_elapsed_since_record += delta
 	time_elapsed += delta
